5bf26014e93ea92ca58b8968eea385bf1d65fa7c
mudit0299 <muditgupta.rocks@gmail.com>
2022-01-20 17:58:46 +0530

    JDBC QUERIES AND CONNECTION

--- JPA AND JDBC/demo/src/main/java/com/example/database/DemoApplication.java
+++ JPA AND JDBC/demo/src/main/java/com/example/database/DemoApplication.java
@@ -1,13 +1,33 @@
 package com.example.database;
 
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.database.jdbc.PersonJdbcDAO;
+
+
+
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
+	
+	private Logger logger=org.slf4j.LoggerFactory.getLogger(this.getClass());
+	@Autowired
+	PersonJdbcDAO dao;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		logger.info("aLL users->{}",dao.findAll());
+		logger.info("user id 10001 ->{}",dao.findById(10001));
+		logger.info("user id 10002 ->{}",dao.deleteById(10002));
 	}
 
 }
---
+++ JPA AND JDBC/demo/src/main/java/com/example/database/entity/Person.java
@@ -0,0 +1,54 @@
+package com.example.database.entity;
+
+import java.sql.Date;
+
+public class Person {
+	
+	private int id;
+	private String name;
+	private String location;
+	private Date birth_date;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public Date getBirth_date() {
+		return birth_date;
+	}
+	public void setBirth_date(Date birth_date) {
+		this.birth_date = birth_date;
+	}
+	public Person() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Person(int id, String name, String location, Date birth_date) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.location = location;
+		this.birth_date = birth_date;
+	}
+	@Override
+	public String toString() {
+		return "\nPerson [id=" + id + ", name=" + name + ", location=" + location + ", birth_date=" + birth_date + "]";
+	}
+	
+	 
+	
+
+}
