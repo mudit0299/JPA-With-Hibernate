cd0cc373aa699eb76d6647d6fff974e5d10f9711
mudit0299 <muditgupta.rocks@gmail.com>
2022-01-20 12:50:32 +0530

    Confgured H2 database and data.sql file

---
+++ JPA AND JDBC/demo/src/main/resources/data.sql
@@ -0,0 +1,13 @@
+create table person
+(
+id integer not null,
+name varchar(255) not null,
+location varchar(255),
+birth_date timestamp,
+primary key(id)
+);
+
+insert into person(id,name,location,birth_date) values (10001,'ranga','Hyderabad',sysdate());
+insert into person(id,name,location,birth_date) values (10002,'james','newyork',sysdate());
+insert into person(id,name,location,birth_date) values (10003,'jatin','delhi',sysdate());
+insert into person(id,name,location,birth_date) values (10004,'shikhar','jj ',sysdate());
