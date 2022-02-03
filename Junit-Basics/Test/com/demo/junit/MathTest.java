6414f271cc330cd249c935ab1fd4ac9c85935748
mudit0299 <muditgupta.rocks@gmail.com>
2022-02-03 21:50:55 +0530

    JUNIT BASICS

---
+++ Junit-Basics/Test/com/demo/junit/MathTest.java
@@ -0,0 +1,14 @@
+package com.demo.junit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MathTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Junit-Basics/src/com/demo/junit/Math.java
@@ -0,0 +1,5 @@
+package com.demo.junit;
+
+public class Math {
+
+}
